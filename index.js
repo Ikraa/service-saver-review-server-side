6cab5729bdeb1d2a4276c49068bbce097141ca3d
munnisaikra <munnisaikra750@gmail.com>
2023-01-09 03:40:09 +0600

    server code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 //userName=Service_Saver
 //password=sIv5sEydkpDFM95x
@@ -25,6 +25,7 @@ const run = async () => {
   try {
     await client.connect();
     const serviceCollection = client.db("Service_Saver").collection("Services");
+    const reviewCollection = client.db("Service_Saver").collection("Review");
 
     app.post("/service", async (req, res) => {
       const data = req.body;
@@ -35,6 +36,40 @@ const run = async () => {
     app.get("/service", async (req, res) => {
       const result = await serviceCollection.find({}).toArray();
       // console.log(object);
+      res.send(result);
+    });
+    app.get("/service/:id", async (req, res) => {
+      const result = await serviceCollection.findOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+    // review
+    app.post("/review", async (req, res) => {
+      const data = req.body;
+
+      const result = await reviewCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/review", async (req, res) => {
+      const id = req.query.serviceid;
+      const result = await reviewCollection.find({ serviceId: id }).toArray();
+      // console.log(object);
+      res.send(result);
+    });
+    app.get("/review/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const result = await reviewCollection.find({ email: email }).toArray();
+
+      res.send(result);
+    });
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await reviewCollection.deleteOne({ _id: ObjectId(id) });
+
       res.send(result);
     });
   } finally {
